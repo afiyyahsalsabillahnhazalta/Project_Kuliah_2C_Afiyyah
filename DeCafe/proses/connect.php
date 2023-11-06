afb5d70b87a78e44b5ecf6d1b917776b052747c5
afiyyahsalsabillahnhazalta <afiyyahsalsabillah08@gmail.com>
2023-11-06 14:55:02 +0700

    update connect

--- DeCafe/proses/connect.php
+++ DeCafe/proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "u837952942_yaya", "ZS[;S#96a#", "db_decafe");
+$conn = mysqli_connect("localhost", "u837952942_yaya", "ZS[;S#96a#", "u837952942_yaya");
 if (!$conn){
     echo "Gagal Koneksi";
 }
